45deab81086f6d758275b2df2c9e190794282f82
Nicolas Kulpa-Hurault <nicolas.kulpa-hurault@hotmail.com>
2023-08-15 17:04:01 +0200

    ajout  de la classe active dans le header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link, useLocation } from 'react-router-dom'; 
 import Logo from '../../assets/logo.png';
 import './Header.scss'
 
 export default function Header() {
+  const location = useLocation();
   return (
-    <header className='header-block'>
-      <Link to="/"> 
-        <img  src={Logo} alt="logo de Kasa" />
-      </Link>
+    <header className='header-block'> 
+      <img  src={Logo} alt="logo de Kasa" />
       <nav>
         <ul className='header-block_nav'>
-            <li><Link to="/">Accueil</Link></li> 
-            <li><Link to="/about">A Propos</Link></li>
+            {/* Add the class 'active' to the Accueil link if the current location is '/' */}
+          <li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Accueil</Link></li>
+          {/* Add the class 'active' to the A Propos link if the current location is '/about' */}
+          <li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>A Propos</Link></li>
         </ul>
       </nav>
     </header>
--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -11,4 +11,6 @@ export default function Error() {
       </section>
     )
   }
+
+
   
